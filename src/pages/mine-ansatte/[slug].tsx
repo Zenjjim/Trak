a56fe3421edb9612e1117f5ba904535258a1f205
Max Torre Schau <max.torre.schau@gmail.com>
2021-02-15 12:26:35 +0100

    fix(Tabbing): Fixed tabbing (ish)

--- src/pages/mine-ansatte/[slug].tsx
+++ src/pages/mine-ansatte/[slug].tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Table, TableCell, TableHead, TableRow } from '@material-ui/core';
+import { Avatar, Box, ButtonBase, IconButton, Table, TableCell, TableHead, TableRow } from '@material-ui/core';
 import { ExpandLess as ExpandLessIcon, ExpandMore as ExpandMoreIcon, Search as SearchIcon, Tune as TuneIcon } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/styles';
 import classNames from 'classnames';
@@ -8,8 +8,7 @@ import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import { useState } from 'react';
 import theme from 'theme';
-import { IEmployee, IProfession } from 'utils/types';
-import { getPhasesWithEmployees } from 'utils/utilFunctions';
+import { IEmployee, IEmployeeTask, IPhase, IProcessTemplate, IProfession } from 'utils/types';
 
 const LOGGED_IN_USER = 1;
 
@@ -151,8 +150,41 @@ const useStyles = makeStyles({
     alignItems: 'flex-end',
     display: 'flex',
     flexDirection: 'row',
+    '&:focus': {
+      outline: `0.1px solid ${theme.palette.text.disabled}`,
+    },
   },
 });
+export const addFinishedTasks = (filteredEmployees: IEmployee[], phase: IPhase) => {
+  filteredEmployees.forEach((employee: IEmployee) => {
+    employee['tasksFinished'] = employee.employeeTask.filter(
+      (employeeTask: IEmployeeTask) => employeeTask.completed && employeeTask.task.phase.title === phase.title,
+    ).length;
+    employee['totalTasks'] = employee.employeeTask.filter((employeeTask: IEmployeeTask) => employeeTask.task.phase.title === phase.title).length;
+  });
+};
+
+export const getPhasesWithEmployees = (processTemplate: IProcessTemplate, myEmployees) => {
+  const displayedEmployees = [];
+  return [
+    ...processTemplate.phases.map((phase: IPhase) => {
+      const employeesWithUncompletedTasks = myEmployees.filter((employee: IEmployee) =>
+        employee.employeeTask.some((employeeTask: IEmployeeTask) => !employeeTask.completed && employeeTask.task.phase.title === phase.title),
+      );
+      const filteredEmployees = employeesWithUncompletedTasks.filter((employee: IEmployee) => {
+        if (displayedEmployees.includes(employee.id)) {
+          return false;
+        } else {
+          displayedEmployees.push(employee.id);
+          return true;
+        }
+      });
+      addFinishedTasks(filteredEmployees, phase);
+
+      return { employees: filteredEmployees, title: phase.title, id: phase.id };
+    }),
+  ];
+};
 const MyEmployees = ({ myEmployees, allPhases }: InferGetStaticPropsType<typeof getStaticProps>) => {
   const classes = useStyles();
   const processTemplate = allPhases[0];
@@ -169,13 +201,17 @@ const MyEmployees = ({ myEmployees, allPhases }: InferGetStaticPropsType<typeof 
           <Typo variant='h2'>{processTemplate.title}</Typo>
         </Box>
         <Box display='flex' justifyContent='flex-end'>
-          <Box className={classNames(classes.pointer, classes.centeringRow)} padding={theme.spacing(2)} tabIndex={0}>
-            <SearchIcon />
-            <Typo variant='body2'>Søk</Typo>
+          <Box className={classNames(classes.pointer, classes.centeringRow)} onClick={() => null} padding={theme.spacing(2)}>
+            <IconButton aria-label='Søk'>
+              <SearchIcon />
+              <Typo variant='body2'>Søk</Typo>
+            </IconButton>
           </Box>
           <Box className={classNames(classes.pointer, classes.centeringRow)} padding={theme.spacing(2)}>
-            <TuneIcon />
-            <Typo variant='body2'>Filter</Typo>
+            <IconButton aria-label='Filter'>
+              <TuneIcon />
+              <Typo variant='body2'>Filter</Typo>
+            </IconButton>
           </Box>
         </Box>
         {phases.map((phase: PhaseCardProps) => {
@@ -207,24 +243,24 @@ const UserRow = ({ employee }: UserRowProps) => {
   const classes = useStyles();
   const typoVariant = 'body2';
   return (
-    <TableRow className={classes.pointer}>
-      <TableCell style={{ width: '37.5rem' }}>
-        <div className={classes.userRow} onKeyDown={(e) => e.key === 'Enter' && null} tabIndex={0}>
+    <TableRow className={classes.pointer} hover>
+      <TableCell>
+        <div className={classes.userRow} tabIndex={0}>
           <Avatar alt={'Logged in user photo'} className={classes.avatar} src={'/dummy_avatar.png'} />
           <Typo variant={typoVariant}>
             {employee.firstName} {employee.lastName}
           </Typo>
         </div>
       </TableCell>
-      <TableCell style={{ width: '18.75rem' }}>
+      <TableCell>
         <Typo variant={typoVariant}>
           <b>{employee.tasksFinished}</b> av <b>{employee.totalTasks}</b>
         </Typo>
       </TableCell>
-      <TableCell style={{ width: '18.75rem' }}>
+      <TableCell>
         <Typo variant={typoVariant}>{employee.profession.title}</Typo>
       </TableCell>
-      <TableCell style={{ width: '18.75rem' }}>
+      <TableCell>
         <Box alignItems='flex-end' display='flex' flexDirection='row'>
           <Avatar alt={'Logged in user photo'} className={classes.avatar} src={'/dummy_avatar.png'} />
           <Typo variant={typoVariant}>
@@ -244,38 +280,36 @@ type PhaseCardProps = {
 };
 const PhaseCard = ({ title, amount, employees }: PhaseCardProps) => {
   const classes = useStyles();
-  const [hidden, setIsHidden] = useState(false);
+  const [hidden, setIsHidden] = useState(employees.length === 0);
   return (
     <>
-      <div
-        className={classNames(classes.centeringRow, classes.pointer)}
-        onClick={() => setIsHidden(!hidden)}
-        onKeyDown={(e) => e.key === 'Enter' && setIsHidden(!hidden)}
-        tabIndex={0}>
-        <Typo variant='h2'>
-          {title} (<b>{amount}</b>)
-        </Typo>
-        {hidden ? <ExpandMoreIcon /> : <ExpandLessIcon />}
-      </div>
-      {employees.length > 0 ? (
-        <Box display={hidden ? 'none' : 'block'}>
+      <ButtonBase focusRipple onClick={() => setIsHidden(!hidden)}>
+        <div className={classNames(classes.centeringRow, classes.pointer)}>
+          <Typo variant='h2'>
+            {title} (<b>{amount}</b>)
+          </Typo>
+          {hidden ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+        </div>
+      </ButtonBase>
+      <Box display={hidden ? 'none' : 'block'}>
+        {employees.length > 0 ? (
           <Table aria-label='Mine ansatte tabell'>
             <TableHead>
               <TableRow>
-                <TableCell size='small'>Navn</TableCell>
-                <TableCell size='small'>Oppgaver gjennomført</TableCell>
-                <TableCell size='small'>Stilling</TableCell>
-                <TableCell size='small'>Ansvarlig</TableCell>
+                <TableCell style={{ width: '37.5rem' }}>Navn</TableCell>
+                <TableCell style={{ width: '18.75rem' }}>Oppgaver gjennomført</TableCell>
+                <TableCell style={{ width: '18.75rem' }}>Stilling</TableCell>
+                <TableCell style={{ width: '18.75rem' }}>Ansvarlig</TableCell>
               </TableRow>
             </TableHead>
             {employees.map((employee) => {
               return <UserRow employee={employee} key={employee.id} />;
             })}
           </Table>
-        </Box>
-      ) : (
-        <Typo variant='body2'>Ingen ansatte i denne fasen</Typo>
-      )}
+        ) : (
+          <Typo variant='body2'>Ingen ansatte i denne fasen</Typo>
+        )}
+      </Box>
     </>
   );
 };

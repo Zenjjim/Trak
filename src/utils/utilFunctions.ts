d12ed1a441b5ccb0c181d3fb019839d144e4f1ae
Max Torre Schau <max.torre.schau@gmail.com>
2021-02-15 12:26:35 +0100

    fix(Tabbing): Fixed tabbing (ish)

--- src/utils/utilFunctions.ts
+++ src/utils/utilFunctions.ts
@@ -1,36 +1,3 @@
-import { IEmployee, IEmployeeTask, IPhase, IProcessTemplate } from './types';
-
 export function capitalizeFirstLetter(text: string) {
   return text.charAt(0).toUpperCase() + text.slice(1);
 }
-
-export const addFinishedTasks = (filteredEmployees: IEmployee[], phase: IPhase) => {
-  filteredEmployees.forEach((employee: IEmployee) => {
-    employee['tasksFinished'] = employee.employeeTask.filter(
-      (employeeTask: IEmployeeTask) => employeeTask.completed && employeeTask.task.phase.title === phase.title,
-    ).length;
-    employee['totalTasks'] = employee.employeeTask.filter((employeeTask: IEmployeeTask) => employeeTask.task.phase.title === phase.title).length;
-  });
-};
-
-export const getPhasesWithEmployees = (processTemplate: IProcessTemplate, myEmployees) => {
-  const displayedEmployees = [];
-  return [
-    ...processTemplate.phases.map((phase: IPhase) => {
-      const employeesWithUncompletedTasks = myEmployees.filter((employee: IEmployee) =>
-        employee.employeeTask.some((employeeTask: IEmployeeTask) => !employeeTask.completed && employeeTask.task.phase.title === phase.title),
-      );
-      const filteredEmployees = employeesWithUncompletedTasks.filter((employee: IEmployee) => {
-        if (displayedEmployees.includes(employee.id)) {
-          return false;
-        } else {
-          displayedEmployees.push(employee.id);
-          return true;
-        }
-      });
-      addFinishedTasks(filteredEmployees, phase);
-
-      return { employees: filteredEmployees, title: phase.title, id: phase.id };
-    }),
-  ];
-};

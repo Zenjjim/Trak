1f2397c745798099e0a44023358bab7107316077
Max Torre Schau <max.torre.schau@gmail.com>
2021-02-18 08:28:37 +0100

    style(autocomplete): Finished new design

--- src/components/TagSelector.tsx
+++ src/components/TagSelector.tsx
@@ -1,20 +1,27 @@
-import { Autocomplete, TextField } from '@material-ui/core';
+import { Autocomplete, createFilterOptions, TextField } from '@material-ui/core';
 import { useState } from 'react';
 import { Control, Controller } from 'react-hook-form';
-import { ITag } from 'utils/types';
+import { ITag, ITask } from 'utils/types';
 import { getUniqueTags } from 'utils/utilFunctions';
+
+type FilterOptions = {
+  id: string;
+  title: string;
+  inputValue?: string;
+  tasks?: ITask[];
+};
 
 export type TagSelectorProps = {
   label: string;
   control: Control<Record<string, unknown>>;
-  options: ITag[];
+  options: FilterOptions[];
   name: string;
 };
 const TagSelector = ({ label, options, control, name }: TagSelectorProps) => {
+  const filter = createFilterOptions<FilterOptions>();
   // TODO
   // If editing a task this will be updated with that data, otherwise empty
-
-  const [tags, setTags] = useState<ITag[]>([]);
+  const [tags, setTags] = useState<FilterOptions[]>([]);
   return (
     <Controller
       as={({ onChange }) => (
@@ -22,8 +29,21 @@ const TagSelector = ({ label, options, control, name }: TagSelectorProps) => {
           autoSelect
           clearOnBlur
           defaultValue={tags}
+          filterOptions={(options, params) => {
+            const filtered = filter(options, params);
+            if (!filtered.length && params.inputValue !== '') {
+              filtered.push({
+                id: null,
+                inputValue: params.inputValue,
+                title: `Legg til "${params.inputValue}"`,
+              });
+            }
+            return filtered;
+          }}
           freeSolo
-          getOptionLabel={(option) => option.title}
+          getOptionLabel={(option) => {
+            return option.title;
+          }}
           handleHomeEndKeys
           multiple
           noOptionsText={'Ingen tags'}
@@ -31,6 +51,9 @@ const TagSelector = ({ label, options, control, name }: TagSelectorProps) => {
             const updatedTags = newValue.map((tag) => {
               if (typeof tag === 'string') {
                 const newTag: ITag = { id: null, title: tag.toLowerCase() };
+                return newTag;
+              } else if (tag.inputValue) {
+                const newTag: ITag = { id: null, title: tag.inputValue.toLowerCase() };
                 return newTag;
               } else {
                 return tag;

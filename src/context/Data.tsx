889b2505c723c0c758e137d91b169b81111b45b2
Zenjjim <zaim.imran@gmail.com>
2021-04-08 10:55:16 +0200

    fix: only get data if logged in

--- src/context/Data.tsx
+++ src/context/Data.tsx
@@ -1,3 +1,4 @@
+import { useSession } from 'next-auth/client';
 import { createContext, useContext } from 'react';
 import useSWR, { responseInterface } from 'swr';
 import { IEmployee, IProcessTemplate, IProfession, ITag } from 'utils/types';
@@ -12,10 +13,22 @@ function useData() {
   return context;
 }
 function DataProvider(props) {
-  const { data: professions }: responseInterface<IProfession[], unknown> = useSWR(`/api/professions`, fetcher);
-  const { data: tags }: responseInterface<ITag[], unknown> = useSWR(`/api/tags`, fetcher);
-  const { data: employees }: responseInterface<IEmployee[], unknown> = useSWR(`/api/employees`, fetcher);
-  const { data: processTemplates }: responseInterface<IProcessTemplate[], unknown> = useSWR(`/api/processTemplates`, fetcher);
+  const [session] = useSession();
+
+  const { data: professions }: responseInterface<IProfession[], unknown> | { data: undefined } = session?.user
+    ? // eslint-disable-next-line react-hooks/rules-of-hooks
+      useSWR(`/api/professions`, fetcher)
+    : { data: undefined };
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { data: tags }: responseInterface<ITag[], unknown> | { data: undefined } = session?.user ? useSWR(`/api/tags`, fetcher) : { data: undefined };
+  const { data: employees }: responseInterface<IEmployee[], unknown> | { data: undefined } = session?.user
+    ? // eslint-disable-next-line react-hooks/rules-of-hooks
+      useSWR(`/api/employees`, fetcher)
+    : { data: undefined };
+  const { data: processTemplates }: responseInterface<IProcessTemplate[], unknown> | { data: undefined } = session?.user
+    ? // eslint-disable-next-line react-hooks/rules-of-hooks
+      useSWR(`/api/processTemplates`, fetcher)
+    : { data: undefined };
   return <DataContext.Provider value={{ professions: professions, tags: tags, employees: employees, processTemplates: processTemplates }} {...props} />;
 }
 export { DataProvider, useData };

210275cc64b55f7e8422e7e55f821a2f776ba7ac
Zenjjim <zaim.imran@gmail.com>
2021-04-27 19:41:59 +0200

    feat(test): update tests

--- __tests__/cron.test.ts
+++ __tests__/cron.test.ts
@@ -1,25 +1,31 @@
 import HttpStatusCode from 'http-status-typed';
-import { createMocks } from 'node-mocks-http';
+import { PageConfig } from 'next';
+import { testApiHandler } from 'next-test-api-route-handler';
 import cronAPI from 'pages/api/cron/phases';
 
 import { employeeFactory } from './factories/employee.factory';
 import { taskFactory } from './factories/task.factory';
 
 const CRON_SECRET = process.env.CRON_SECRET;
+const cronAPIHandler: typeof cronAPI & { config?: PageConfig } = cronAPI;
 
 const cron_test = async (start, end) => {
   let loop = new Date(start);
   while (loop <= end) {
     Date.now = jest.fn(() => loop.valueOf());
-    const { req, res } = createMocks({
-      method: 'POST',
-      headers: {
-        CRON_SECRET: CRON_SECRET,
+    await testApiHandler({
+      handler: cronAPIHandler,
+      test: async ({ fetch }) => {
+        const res = await fetch({
+          method: 'POST',
+          headers: {
+            CRON_SECRET: CRON_SECRET,
+          },
+        });
+        expect(res.status).toBe(HttpStatusCode.OK);
       },
     });
 
-    await cronAPI(req, res);
-    expect(res._getStatusCode()).toBe(HttpStatusCode.OK);
     const newDate = loop.setDate(loop.getDate() + 1);
     loop = new Date(newDate);
   }
--- __tests__/employeeTask.test.ts
+++ __tests__/employeeTask.test.ts
@@ -1,11 +1,14 @@
 import HttpStatusCode from 'http-status-typed';
 import prisma from 'lib/prisma';
-import { createMocks } from 'node-mocks-http';
+import { PageConfig } from 'next';
+import { testApiHandler } from 'next-test-api-route-handler';
 import employeeTaskAPI from 'pages/api/employeeTasks/[id]';
 
 import { employeeTaskFactory } from './factories/employeeTask.factory';
 
 describe('/api/employeeTask', () => {
+  const employeeTaskAPIHandler: typeof employeeTaskAPI & { config?: PageConfig } = employeeTaskAPI;
+
   let employeeTask;
   beforeAll(async () => {
     employeeTask = await employeeTaskFactory();
@@ -18,34 +21,41 @@ describe('/api/employeeTask', () => {
 
   describe('Get task', () => {
     test('Get employeeTask', async () => {
-      const { req, res } = createMocks({
-        method: 'GET',
-        query: { id: employeeTask.id },
+      await testApiHandler({
+        handler: employeeTaskAPIHandler,
+        params: { id: employeeTask.id },
+        test: async ({ fetch }) => {
+          const res = await fetch({
+            method: 'GET',
+          });
+          expect(res.status).toBe(HttpStatusCode.OK);
+        },
       });
-
-      await employeeTaskAPI(req, res);
-
-      expect(res._getStatusCode()).toBe(HttpStatusCode.OK);
     });
   });
 
   describe('Complete task', () => {
     test('Complete employeeTask', async () => {
-      const { req, res } = createMocks({
-        method: 'PUT',
-        query: { id: employeeTask.id },
-        body: {
-          data: {
-            completed: true,
-            dueDate: employeeTask.dueDate,
-            responsibleId: employeeTask.responsibleId,
-          },
+      await testApiHandler({
+        handler: employeeTaskAPIHandler,
+        params: {
+          id: employeeTask.id,
+        },
+        test: async ({ fetch }) => {
+          const res = await fetch({
+            method: 'PUT',
+            headers: {
+              'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+              completed: true,
+              dueDate: employeeTask.dueDate,
+              responsibleId: employeeTask.responsibleId,
+            }),
+          });
+          expect(res.status).toBe(HttpStatusCode.OK);
         },
       });
-
-      await employeeTaskAPI(req, res);
-
-      expect(res._getStatusCode()).toBe(HttpStatusCode.OK);
     });
   });
 });
